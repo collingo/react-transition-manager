86426e51537ee194aa32f336be6b2d68d589afff
Nick Collings <nick@collingo.com>
2015-07-20 12:21:48 +0100

    Release 1.2.2 - remove console log

--- src/clone-with-classes.js
+++ src/clone-with-classes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import classnames from 'classnames/dedupe';
 
 export default function cloneWithClasses(element, classes) {
-  console.log(element, classes);
   let currentClasses = element.props.className ? element.props.className.split(' ') : [];
   let newClasses = classnames.apply(null, currentClasses.concat(classes));
   const newElement = React.cloneElement(element, {
